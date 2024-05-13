4a3ee17fe7b5886caa85ccffffa5c789744e928b
mmrahorovic <mmrahorovic@hotmail.com>
2024-05-14 00:58:06 +0100

    [vgg10]: added specialize layers config file to implement MVU in HLS

--- build/vgg10-radioml/build.py
+++ build/vgg10-radioml/build.py
@@ -110,6 +110,7 @@ for platform_name in platforms_to_build:
         shell_flow_type=shell_flow_type,
         vitis_platform=vitis_platform,
         folding_config_file="folding_config/%s_folding_config.json" % platform_name,
+        specialize_layers_config_file="specialize_layers_config/vgg10_specialize_layers.json",
         split_large_fifos=True,
         standalone_thresholds=True,
         # enable extra performance optimizations (physopt)
